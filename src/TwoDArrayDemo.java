d990bebca49afb6213824368a15e7e937f56f377
Josh Greco <greco.joshua@gmail.com>
2018-01-29 21:38:01 -0600

    Demo

--- src/TwoDArrayDemo.java
+++ src/TwoDArrayDemo.java
@@ -3,8 +3,28 @@
 public class TwoDArrayDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[][] table = new int[5][10];
+		
+		//load the 2D array with data
+		//r = row
+		//c = column
+		for(int r = 0; r < table.length; r++) {
+			
+			//sub-row
+			for(int c = 0; c < table[r].length; c++) {
+				
+				table[r][c] = r * 10 + c;
+			}
+		}
+		
+		//print the 2D array data
+		for(int r = 0; r < table.length; r++) {
+			//sub-row
+			for(int c = 0; c < table[r].length; c++) {
+				System.out.println(table[r][c]);
+			}
+		}
 	}
 
 }
