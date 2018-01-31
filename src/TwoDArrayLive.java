613d8f64e8b9c2552f0d0edb4f0f5cc251fc0e48
Josh Greco <greco.joshua@gmail.com>
2018-01-30 20:36:04 -0600

    My computer restarted for some unknown reason. I am adding again to be safe.

--- src/TwoDArrayLive.java
+++ src/TwoDArrayLive.java
@@ -3,17 +3,38 @@ public class TwoDArrayLive {
 
 	public static void main(String[] args) {
 		
-		int [][]grid = new int[5][6];
+//		int [][]grid = new int[5][6];
+//		
+//		for(int r = 0; r < grid.length; r++) {
+//			for(int c = 0; c < grid[r].length; c++) {
+//				grid[r][c] = (int)(Math.random() * 10);
+//				System.out.print(grid[r][c] + "\t");
+//			}
+//			System.out.println();
+//		}
 		
+		int[]nums = {1,2,3,4,5,6,7,8,9};
+		
+		int [][]grid = new int[3][3];
+		int i = 0;
+
 		for(int r = 0; r < grid.length; r++) {
+			if(r % 2 == 0) { // if row is even
 			for(int c = 0; c < grid[r].length; c++) {
-				grid[r][c] = (int)(Math.random() * 10);
+				grid[r][c] = nums[i];
 				System.out.print(grid[r][c] + "\t");
+				i++;
+			}
+			}
+			else {
+				for(int c = grid[r].length - 1; c >= 0; c--) {
+					grid[r][c] = nums[i];
+					System.out.print(grid[r][c] + "\t");
+					i++;
+			}
 			}
 			System.out.println();
-		}
-		
-
+			}
+		}		
 	}
 
-}
